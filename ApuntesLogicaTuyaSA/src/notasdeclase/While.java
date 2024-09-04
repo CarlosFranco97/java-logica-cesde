96cace74948658a01a9bf0fb47864e99d0dc0ba3
CarlosFranco97 <carlosfranco63.cf@gmail.com>
2024-09-04 17:53:51 -0500

    Se hacen ejercicios en clase, utilizando while

--- ApuntesLogicaTuyaSA/src/notasdeclase/While.java
+++ ApuntesLogicaTuyaSA/src/notasdeclase/While.java
@@ -28,7 +28,7 @@ public class While {
            System.out.println("Seleccione 1. para crear un usuario" + "\n" +
                     "Seleccione 2. Iniciar sesion" + "\n" +
                     "Seleccione 3. para calcular el salario " + "\n" +
-                    "Seleccione 4 validar categoria de salario" + "\n" +
+                    "Seleccione 4 validar categoroia de salario" + "\n" +
                     "Seleccione 5. para ver datos del usuario" + "\n" +
                     "6. para salir"
             );
@@ -82,22 +82,19 @@ public class While {
                     break;
 
                 case 4:
-                    System.out.println("Validar categoria del salario");
-                    System.out.println(salarioNeto);
-                    if (salarioNeto <= 2600000 ) {
+                    System.out.println("Validar categorias del usuario");
+
+                    if (salarioNeto * 2 <= 2600000 ) {
                         System.out.println("El salario es de tipo A");
-                    } else if (salarioNeto >= 2600000 && salarioNeto <= 5200000){
+                    } else if (salarioNeto <= 3500000){
                         System.out.println("El salario es de tipo B");
-                    } else {
+                    } else if (salarioNeto >= 4000000) {
                         System.out.println("El salario es de tipo C");
                     }
+
                     break;
                 case 5:
-                    System.out.println( "usuario " + userCreated + "\n" +
-                                    "correo " + correoCreated + "\n" +
-                                    "el salario es " + salarioNeto
-                        );
-                    break;
+                    System.out.println("Ver datos del usuario");
                 case 6:
                     init = 0;
                     break;
