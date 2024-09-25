c89edda388eaf9bea25379a06b548db28f85bde3
CarlosFranco97 <carlosfranco63.cf@gmail.com>
2024-09-25 17:53:23 -0500

    Se agrega ejercicio, por terminar TiendaAlex

---
+++ ApuntesLogicaTuyaSA/src/ejercicios/TiendaAlex.java
@@ -0,0 +1,32 @@
+/*package ejercicios;
+
+public class TiendaAlex {
+    String[] [] [] [] [] arrProductos = new String[5][5][5][5][5];
+    int idProducto;
+    String nombreProducto;
+    double precio;
+    int cantidad;
+    boolean estado;
+
+    /* public static void main(String[] args) {
+        guardarVector();
+    }
+     */
+   /* public static guardarVector(int idProducto, String nombreProducto, double precio, int cantidad, boolean estado) {
+        for(int i = 0; i < arrProductos.length; i++) {
+            System.out.println("ingrese datos del producto");
+            for(int j = 0; j < arrProductos[i].length; j++) {
+                for(int k = 0; k < arrProductos[i][j].length; k++) {
+                    for(int h = 0; h < arrProductos[i][j][k].length; h++) {
+                        for(int z = 0; z < arrProductos[i][j][k][h].length; z++) {
+                            System.out.println(
+                                    arrProductos[i][j][k][h][z].length()
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+*/
