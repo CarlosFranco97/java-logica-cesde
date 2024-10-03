96d6de787179151760ff649726b52955516adea9
CarlosFranco97 <carlosfranco63.cf@gmail.com>
2024-10-03 07:56:32 -0500

    Se modifica la tienda alex

--- ApuntesLogicaTuyaSA/src/ejercicios/TiendaAlex.java
+++ ApuntesLogicaTuyaSA/src/ejercicios/TiendaAlex.java
@@ -3,24 +3,25 @@ package ejercicios;
 import java.util.Scanner;
 
 public class TiendaAlex {
-    //arreglo 5x5;
+    //arreglo 5x5 [5][5]
     static Scanner sc = new Scanner(System.in);
-   static String[] [] [] [] [] matrizProducto = new String[5][5][5][5][5]; //{{{{ {}, {}, {} }}}}
-   static String[] producto = new String[5];
+    static String[][] matrizProducto = new String[5][5];
+    static String[] producto = new String[5];
     static int idProducto;
     static String nombreProducto;
-    static float precio;
+    static double precio;
     static int cantidad;
     static boolean estado;
 
     public static void main(String[] args) {
-        System.out.println("Ingrese datos del producto");
-         crearProductos();
-        System.out.println(idProducto + ", " + nombreProducto + ", " + precio + ", " + cantidad + ", " + estado);
+        agregarMatrizProducto();
+        listarPreciosProducto();
+
     }
 
-    public static void crearProductos(){
-        for(int i = 0; i < producto.length; i++) {
+    public static String[] crearProductos(){
+
+        /*  for(int i = 0; i < producto.length; i++) {
             idProducto = i;
 
             System.out.println("Ingrese nombre del producto");
@@ -34,6 +35,48 @@ public class TiendaAlex {
 
             estado = true;
 
+        } */
+
+
+
+        System.out.println("ingrese el id del producto");
+        idProducto = sc.nextInt();
+        producto[0] = idProducto + "";
+        sc.nextLine();
+
+        System.out.println("Ingrese nombre del producto");
+        nombreProducto = sc.nextLine();
+        producto[1] = nombreProducto;
+
+        System.out.println("Ingresa precio");
+        precio = sc.nextDouble();
+        producto[2] = precio + "";
+
+
+        System.out.println("Ingrese la cantidad");
+        cantidad = sc.nextInt();
+        producto[3] = cantidad + "";
+
+        estado = true;
+        producto[4] = estado + "";
+
+        return producto;
+    }
+
+    public static void agregarMatrizProducto() {
+        int i = 0;
+        while (i < matrizProducto.length){
+            matrizProducto[i] = crearProductos();
+            i++;
+        }
+    }
+
+    public static void listarPreciosProducto() {
+        for(int i = 0; i < matrizProducto.length; i++) {
+            for(int j = 0; j < matrizProducto[i].length; j++) {
+                System.out.print(matrizProducto[i][j]);
+                System.out.println("\n");
+            }
         }
     }
 
